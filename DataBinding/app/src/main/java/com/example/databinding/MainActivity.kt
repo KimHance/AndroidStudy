f1fbd2866fc8572b4f366f98e3abf122fdfb4963
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-03-13 22:34:52 +0900

    연습 1

--- DataBinding/app/src/main/java/com/example/databinding/MainActivity.kt
+++ DataBinding/app/src/main/java/com/example/databinding/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
+
+        val user =User ("김현수",25)
+        binding.user = user
     }
 }
---
+++ DataBinding/app/src/main/java/com/example/databinding/User.kt
@@ -0,0 +1,6 @@
+package com.example.databinding
+
+data class User(
+    val name : String,
+    val age : Int
+)
